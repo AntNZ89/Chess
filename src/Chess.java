9888c7edef79ce30c450c6eb7b3b026f95030de8
AntND <antonlenz508@gmail.com>
2025-12-19 20:25:53 +0100

    update

--- src/Chess.java
+++ src/Chess.java
@@ -258,7 +258,12 @@ public class Chess extends Frame {
 
     public static String printBoard(Figure[][] board){
         String s = "\n";
+        for (int i = 0 ; i < 8 ; i++){
+            s += "  " + i + "   ";
+        }
+        s += "\n";
         for (int i = 0 ; i < board.length ; i++){
+            s += i + " ";
             for (int j = 0 ; j < board[0].length ; j++){
                 if (board[i][j] == null){
                     s += "_____";
